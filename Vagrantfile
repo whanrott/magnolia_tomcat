abaacb1454e36b9b632618489e53c557d0ff1e17
whanrott <eart0208@nexus.ox.ac.uk>
2016-06-21 17:18:55 +0100

    added downloads for magnolia versions

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@
 Vagrant.configure(2) do |config|
 	config.vm.hostname = 'medlearn.local'
 	config.vm.box = "ubuntu/trusty64"
-	config.vm.network "private_network", ip: "192.168.33.10"
+	config.vm.network "private_network", ip: "192.168.33.15"
 	config.vm.synced_folder "./medlearn/", "/var/lib/tomcat", create: true, owner: 'www-data', group: 'www-data'
 	config.vm.provider "virtualbox" do |vb|
 		vb.name = "medlearn"
--- provision.sh
+++ provision.sh
@@ -5,10 +5,21 @@ apt-get update
 apt-get -y upgrade
 echo "Installing required packages..."
 apt-get -y install \
+	openjdk-7-jre-headless \
 	git \
 	unzip \
 	npm
-wget -q http://downloads.sourceforge.net/project/magnolia/magnolia/Magnolia%20CE%204.4.2/magnolia-tomcat-bundle-4.4.2-tomcat-bundle.tar.gz -d /var/lib/tomcat
-wget -q http://downloads.sourceforge.net/project/magnolia/magnolia/Magnolia%20CE%205.4.7/magnolia-tomcat-bundle-5.4.7-tomcat-bundle.tar.gz -d /var/lib/tomcat
-tar zxvf magnolia-tomcat-bundle-4.4.2-tomcat-bundle.tar.gz
-tar zxvf magnolia-tomcat-bundle-5.4.7-tomcat-bundle.tar.gz
+sudo -u www-data wget --quiet http://downloads.sourceforge.net/project/magnolia/magnolia/Magnolia%20CE%204.4.2/magnolia-tomcat-bundle-4.4.2-tomcat-bundle.tar.gz -d /var/lib/tomcat
+sudo -u www-data wget --quiet http://downloads.sourceforge.net/project/magnolia/magnolia/Magnolia%20CE%205.4.7/magnolia-community-demo-bundle-5.4.7-tomcat-bundle.tar.gz -d /var/lib/tomcat
+cd /var/lib/tomcat
+sudo -u www-data tar zxvf magnolia-tomcat-bundle-4.4.2-tomcat-bundle.tar.gz
+sudo -u www-data tar zxvf magnolia-community-demo-bundle-5.4.7-tomcat-bundle.tar.gz
+
+echo "initialise magnolia 4.4.2. NB: there is an error in setenv.sh - the -a switch should be -e"
+sudo -u www-data mv magnolia-4.4.2/apache-tomcat-6.0.29/* .
+
+sudo -u www-data sh bin/setenv.sh
+sudo -u www-data sh bin/magnolia_control.sh start
+
+echo "cleanup tasks"
+sudo updatedb
